776c43f2b16f956acb438b82e81af7a359652846
samed20080208 <158290645+samed20080208@users.noreply.github.com>
2025-10-22 08:54:06 +0400

    Refactor Task1.cpp for cleaner output syntax
    -remove comments
    -add using namespace std

--- Task1.cpp
+++ Task1.cpp
@@ -1,9 +1,8 @@
-// Online C++ compiler to run C++ program online
+
 #include <iostream>
-
+using namespace std;
 int main() {
-    // Write C++ code here
-    std::cout << "Try programiz.pro";
-
+   cout << "Try programiz.pro";
     return 0;
 }
+
